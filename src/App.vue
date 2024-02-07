0875adba6f0ca0996c576bb5f86bdd1557fe9152
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-02-07 13:34:30 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/simple">Simple</RouterLink>
+      <RouterLink to="/maisons">Maisons</RouterLink>
+      <RouterLink to="/login-logout">Connexion</RouterLink>
     </nav>
   </header>
   <Suspense> 
--- src/pages/login-logout.vue
+++ src/pages/login-logout.vue
@@ -6,7 +6,7 @@ import { supabase, user } from '../supabase';
     <button v-if="user" @pointerdown="supabase.auth.signOut()">
         Se déconnecter ({{user.email}})
     </button>
-    <button v-else @pointerdown="supabase.auth.signIn({provider: 'github'})">
+    <button v-else @pointerdown="supabase.auth.signInWithOAuth({provider: 'github'})">
         Se connecter avec Github
     </button>
 </div>
