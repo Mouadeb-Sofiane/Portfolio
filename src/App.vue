80f90ec69ee1eaf3aa5d3366b0af16e0ff646332
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-02-07 15:11:39 +0100

    card visible avec id sur le lien

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/simple">Simple</RouterLink>
       <RouterLink to="/maisons">Maisons</RouterLink>
+      <RouterLink to="/maisons/edit">Créer des maisons</RouterLink>
       <RouterLink to="/login-logout">Connexion</RouterLink>
     </nav>
   </header>
--- src/pages/maisons/edit/[[id]].vue
+++ src/pages/maisons/edit/[[id]].vue
@@ -1,35 +1,9 @@
 <script setup lang="ts">
-import { ref } from "@vue/reactivity";
-import AfficheMaison from "@/components/AfficheMaison.vue";
-import { FormKit } from "@formkit/vue";
-import { supabase } from "@/supabase";
-
-const maison = ref ({});
-
-async function upsertMaison(dataForm: any, node: { setErrors: (arg0: any[]) => void; }) {
- const { data, error } = await supabase.from("Maison").upsert(dataForm);
- if (error) node.setErrors([error.message])
-}
+import Form from "@/components/Form.vue";
 </script>
 
 <template>
     <div>
-        <div class="p-2">
-            <h2 class="text-2xl">
-                Résultat (Prévisualisation)
-            </h2>
-            <AfficheMaison v-bind="maison" />
-        </div>
-        <div class="p-2">
-            <FormKit @submit="upsertMaison" type="form" v-model="maison">
-                <FormKit name="nomMaison" label="nom de la maison" />
-                <FormKit name="adresse" label="adresse" />
-                <FormKit name="prix" type="number" label="prix" />
-                <FormKit name="favori" type="checkbox" label="mettre en valeur" />
-                <FormKit name="nbrChambres" type="number" label="nombre de chambres" />
-                <FormKit name="nbrSDB" type="number" label="nombre de salle de bain" />
-                <FormKit name="surface" label="surface" />
-            </FormKit>
-        </div>
+        <Form />
     </div>
 </template>
