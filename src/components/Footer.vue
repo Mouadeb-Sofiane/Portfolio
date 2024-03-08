c9ec5f1a5277581fcebee4a447c395a5bb12b340
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-03-08 16:29:24 +0100

    footer finish

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,19 +9,17 @@
 <template>
     <footer class="bg-primary py-4 text-secondary">  
       <div class="bg-gray-800 p-4 flex items-center justify-between">
-        <div class="flex space-x-4">
-          <p class="text-gray-100"><a href="https://mail.google.com/mail/u/1/#inbox?compose=GTvVlcSHwrvPxPSCbQHKdKsWdRxwPqhdRJXLzQNMVlCcjbHTCXDbtDgjNtlGZdxxJGbhWCqlbqlzq" class="hover:text-gray-400">Mon email </a></p>
-          <p class="text-gray-100"><a href="tel:0783889373" class="hover:text-gray-400">Mon téléphone</a></p>
-        </div>
         <div class="flex space-x-12">
           <a href="https://www.linkedin.com/in/sofiane-mouadeb-1b2b78257/" class="text-white hover:text-gray-400"><IconLinkedin /></a>
           <a href="https://www.behance.net/sofianemouadeb" class="text-white hover:text-gray-400"><IconBehance /></a>
           <a href="https://github.com/Mouadeb-Sofiane" class="text-white hover:text-gray-400"><IconGithub /></a>
         </div>
         <p class="text-gray-400">&copy; 2024 Sofiane Mouadeb. Portfolio personnel.</p>
+        <div class="flex space-x-4">
+          <p class="text-gray-100"><a href="https://mail.google.com/mail/u/1/#inbox?compose=GTvVlcSHwrvPxPSCbQHKdKsWdRxwPqhdRJXLzQNMVlCcjbHTCXDbtDgjNtlGZdxxJGbhWCqlbqlzq" class="hover:text-gray-400">Mon email </a></p>
+          <p class="text-gray-100"><a href="tel:0783889373" class="hover:text-gray-400">Mon téléphone</a></p>
+        </div>
       </div>
-
-
     </footer>
   </template>
   
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,10 +1,138 @@
 <script setup lang="ts">
-  import AfficheSingle from '@/components/AfficheSingle.vue';
+import { ref, onMounted } from 'vue'
 </script>
 
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="bg">
+    <div class="lightning">
+    </div>
+    <div class="rain">
+ </div>
   </div>
-  <AfficheSingle />
 </template>
+<style>
+* {
+  margin: 0;
+}
+
+.bg {
+  width: 100wh;
+  height: 100vh;
+  background: url("https://images.unsplash.com/photo-1608939070588-29794905e761?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y3liZXJwdW5rJTIwY2l0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60")
+    center / 100% auto no-repeat;
+  animation: color-change 10s linear infinite;
+  position: relative;
+}
+
+.bg::after {
+  content: "";
+  display: inline-block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  opacity: 0;
+  animation: flash 4s linear infinite;
+}
+
+.lightning {
+  position: absolute;
+  top: 20px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 80px;
+  height: 144px;
+  opacity: 0;
+  animation: lightning 4s linear infinite;
+
+  background: url("https://www.freepnglogos.com/uploads/lightning-png/download-lightning-png-transparent-image-and-clipart-2.png")
+    top center / 100% auto no-repeat;
+}
+
+.rain {
+  height: 100vh;
+  background: url("https://www.freeiconspng.com/thumbs/rain-png/rain-png-transparent-9.png");
+  /*   filter:  contrast(400%); */
+  animation: rain 0.5s linear infinite;
+}
+
+@keyframes rain {
+  0% {
+    background-position: 0% 0%;
+  }
+  100% {
+    background-position: -20% 100%;
+  }
+}
+
+@keyframes color-change {
+  0%,
+  100% {
+    filter: hue-rotate(0deg);
+  }
+  50% {
+    filter: hue-rotate(360deg);
+  }
+}
+
+@keyframes flash {
+  0% {
+    opacity: 0;
+  }
+  10% {
+    opacity: 0;
+  }
+  12% {
+    opacity: 0.3;
+  }
+  14% {
+    opacity: 0;
+  }
+  20% {
+    opacity: 0;
+  }
+  22% {
+    opacity: 0.6;
+  }
+  26% {
+    opacity: 0;
+  }
+}
+
+@keyframes lightning {
+  0% {
+    height: 0px;
+  }
+  8% {
+    height: 0px;
+    opacity: 0;
+  }
+  10% {
+    opacity: 1;
+    height: 88px;
+  }
+  11% {
+    opacity: 0;
+    height: 88px;
+  }
+
+  18% {
+    height: 0px;
+    opacity: 0;
+  }
+  20% {
+    opacity: 1;
+    height: 144px;
+  }
+  25% {
+    opacity: 1;
+    height: 144px;
+  }
+  40% {
+    opacity: 0;
+  }
+}
+</style>
