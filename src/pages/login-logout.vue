178e49024531adbdfe5dfac3261776993ff13a1a
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-03-25 14:38:13 +0100

    accès pour ajouter des projet

--- src/pages/login-logout.vue
+++ src/pages/login-logout.vue
@@ -3,8 +3,11 @@ import { supabase, user } from '../supabase';
 </script>
 
 <template>
-  <div class="mt-10 mb-10">
-    <button v-if="user" @click="supabase.auth.signOut()">
+  <div class="text-center mt-24">
+  Clique sur le bouton pour te connecter avec Github
+  </div>
+  <div class="mt-10 mb-10 text-center bg-primary text-secondary ml-72 mr-72 rounded-full">
+    <button v-if="user" @click="supabase.auth.signOut()" >
       Se déconnecter ({{user.email}})
     </button>
     <button v-else @click="supabase.auth.signInWithOAuth({ provider: 'github', options: { redirectTo: 'https://sofiane.mouadeb/projet/edit' } })">
@@ -12,6 +15,6 @@ import { supabase, user } from '../supabase';
     </button>
   </div>
   <div>
-    <p style="font-family: 'Viga'" class="text-center">Une fois connecté aller sur ce lien pour ajouter des projets. https://sofiane.mouadeb/projet/edit</p>
+    <p style="font-family: 'Viga'" class="text-center mt-24 mb-40">Une fois connecté aller sur ce lien pour ajouter des projets. https://sofiane.mouadeb/projet/edit</p>
   </div>
 </template>
