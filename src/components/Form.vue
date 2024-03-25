d96a0f6961deb40b27a9fd83725dedbfe9332117
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-03-25 12:42:44 +0100

    form vue

---
+++ src/components/Form.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+import AfficheCard from "@/components/AfficheCard.vue";
+import { FormKit } from "@formkit/vue";
+import { supabase } from "@/supabase";
+import { useRouter, useRoute } from "vue-router";
+
+const router = useRouter();
+const projet = ref ({});
+const route = useRoute('/projets/edit/[[id]]');
+
+async function upsertProjet(dataForm: any, node: { setErrors: (arg0: any[]) => void; }) {
+    const { data, error } = await supabase.from("Card").upsert(dataForm).select("id");
+    if (error) node.setErrors([error.message])
+    else {
+        console.log("data :",data);
+        router.push({name:"/projets/edit/[[id]]", params:{id: data[0].id}});
+    }
+}
+
+if (route.params.id) {
+    const { data, error } = await supabase.from("Card").select("*").eq("id", route.params.id).single();
+    if (error) console.error(error);
+    else projet.value = data;
+}
+</script>
+
+<template>
+    <div>
+        <div class="p-2">
+            <h2 class="text-2xl">
+                Résultat (Prévisualisation)
+            </h2>
+            <AfficheCard v-bind="projet" />
+        </div>
+        <div class="p-2">
+            <FormKit @submit="upsertProjet" type="form" v-model="projet"
+            :config="{
+                classes: {
+                    input: 'p-1 rounded border-gray-300 shadow-sm border',
+                    label: 'text-gray-600 italic',
+                    outer: 'py-2',
+                    },
+                }">
+                <FormKit name="titre_film" label="Titre du film" />
+                <FormKit name="description_film" label="Description du film" />
+                <FormKit name="note_film" type="number" label="Note /5" />
+                <FormKit name="image_film" label="Images du film" />
+                <FormKit name="date_film" type="date" label="Date du film" />
+                <FormKit name="temps_film" label="Temps du film" />
+            </FormKit>
+        </div>
+    </div>
+</template>
--- src/pages/projets/edit/[[id]].vue
+++ src/pages/projets/edit/[[id]].vue
@@ -1,49 +1,7 @@
 <script setup lang="ts">
-import { ref } from "@vue/reactivity";
-import AfficheCard from "@/components/AfficheCard.vue";
-import { FormKit } from "@formkit/vue";
-import { supabase } from "@/supabase";
-import { useRouter, useRoute } from "vue-router";
-const router = useRouter();
-const projet = ref ({});
-
-const route = useRoute('/projets/edit/[[id]]');
-async function upsertCard(dataForm: any, node: { setErrors: (arg0: any[]) => void; }) {
-    const { data, error } = await supabase.from("Card").upsert(dataForm).select("id");
-    if (error) node.setErrors([error.message])
-    else {
-        console.log("data :",data);
-        router.push({name:"/projets/edit/[[id]]", params:{id: data[0].id}});
-    }
-}
-if (route.params.id) {
-    const { data, error } = await supabase.from("Card").select("*").eq("id", route.params.id).single();
-    if (error) console.error(error);
-    else projet.value = data;
-}
+    import Form from "@/components/Form.vue";
 </script>
 
 <template>
-    <div>
-        <div class="p-2">
-            <h2 class="text-2xl">
-                Résultat (Prévisualisation)
-            </h2>
-            <AfficheCard v-bind="projet" />
-        </div>
-        <div class="p-2">
-            <FormKit @submit="upsertCard" type="form" v-model="projet"
-            :config="{
-                classes: {
-                    input: 'p-1 rounded border-gray-300 shadow-sm border',
-                    label: 'text-gray-600 italic',
-                    outer: 'py-2',
-                    },
-                }">
-                <FormKit name="nom_projet" label="Nom du projet" />
-                <FormKit name="description_projet" label="Description du projet" />
-
-            </FormKit>
-        </div>
-    </div>
+    <Form />
 </template>
